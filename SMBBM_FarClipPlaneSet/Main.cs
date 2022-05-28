2f033f414da0e93b9eee61a67fe7d08e0819b900
Bradl <Bradl@DESKTOP-KV9EUMA>
2022-05-27 21:37:57 -0500

    Add project files.

---
+++ SMBBM_FarClipPlaneSet/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flash2;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SMBBM_FarClipPlane
+{
+    public static class Main
+    {
+        private static float farClipPlane = 300;
+        private static MgMapCamera mapCam = null;
+
+        public static void OnModLoad(Dictionary<string, object> settings)
+        {
+            farClipPlane = (float) settings["farClipPlane"];
+        }
+
+        public static void OnModUpdate()
+        {
+            // change map camera settings
+            if (mapCam != null)
+            {
+                mapCam.m_Camera.farClipPlane = farClipPlane;
+            }
+            // look for map camera
+            else
+            {
+                mapCam = Object.FindObjectOfType<MgMapCamera>();
+            }
+        }
+    }
+}   
